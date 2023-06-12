ce619c218c418557955c441a3f8045f125b8d8e3
raman martynau <yaretsroman@gmail.com>
2023-06-12 14:07:05 +0300

    added changes

---
+++ src/components/header-banner/header-banner.js
@@ -0,0 +1,12 @@
+import './header-banner.scss';
+import Swiper, { Autoplay, Navigation } from 'swiper';
+
+new Swiper('.header-banner', {
+  modules: [Autoplay, Navigation],
+  speed: 1000,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  slidesPerView: 1,
+});
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+import '@fortawesome/fontawesome-free/js/all';
+import 'swiper/css';
+
 import './main.scss';
-import '.'
-import '.'
-import './components/form.scss';
+import './components/typography/typography.scss';
+import './components/container.scss';
 import './components/form/form';
 import './components/header/header';
+import './components/navigation/navigation';
+import './components/header-banner/header-banner';
+import './components/button/button';
+import './components/top-brands/top-brands';
+import './components/hot-categories/hot-categories';
+import './components/tabs/tabs';
+import './components/card/card';
+import './components/popup/popup';
 
 console.log('Hello IT-academy student 🚀');
